3c8798780ad1d078adbecfc384b825caf0544874
Shane Staret <shanestaret@gmail.com>
2018-03-11 20:34:14 -0400

    Added init, update, render, run, start, and stop methods to Game class

--- src/game/twodgame/Game.java
+++ src/game/twodgame/Game.java
@@ -5,13 +5,58 @@ import game.twodgame.game.twodgame.display.Display;
 /**
  * Created by Shane on 3/11/2018.
  */
-public class Game {
+public class Game implements Runnable {
     private Display display;
+    private String title;
     private int width, height;
+    private boolean running = false;
+    private Thread thread;
 
     public Game(String title, int width, int height) {
         this.width = width;
         this.height = height;
+        this.title = title;
+    }
+
+    public void init() {
         display = new Display(title, width, height);
+    }
+
+    private void update() {
+
+    }
+
+    private void render() {
+
+    }
+
+    public void run() {
+        init();
+
+        while(running) {
+            update();
+            render();
+        }
+
+        stop();
+    }
+
+    public synchronized void start() {
+        if(!running) {
+            running = true;
+            thread = new Thread(this);
+            thread.start();
+        }
+    }
+
+    public synchronized void stop() {
+        if(running) {
+            running = false;
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
